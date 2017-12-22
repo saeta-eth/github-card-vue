f015c4b0760a98ed9674165d32c281a1184b1014
slorenzo <SebastianLorenzo@gmail.com>
2017-12-22 15:13:58 -0300

    Added Home and GithubCard components

---
+++ src/components/GithubCard.vue
@@ -0,0 +1,20 @@
+<template v-if="user">
+  <div>
+    <h2>
+      Details
+    </h2>
+    <img width="250" height="250" :src="user.avatar_url" />
+    <p>{{ user.name }}</p>
+    <p>Public Repositories: {{ user.public_repos }}</p>
+    <a v-bind:href="user.blog"> {{ user.blog }} </a>
+    <p>{{ user.location }}</p>
+    <p>{{ user.bio }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GithubCard',
+  props: ['user'],
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-github-card">
+    <h1> Github Card</h1>
+
+    <div class="input-github-card">
+      <input :value="searchKey" @input="update" @keyup.enter="searchAction" placeholder="Github User">
+    </div>
+    <github-card :user="githubUser"></github-card>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex';
+import GithubCard from './GithubCard';
+
+export default {
+  name: 'Home',
+  components: {
+    GithubCard,
+  },
+  methods: {
+    searchAction() {
+      if (this.searchKey.length) {
+        this.$store.dispatch('searchGithubUser', this.searchKey);
+      }
+    },
+    update(e) {
+      this.$store.dispatch('updateSearchKey', e.target.value);
+    },
+  },
+  computed: {
+    ...mapGetters({
+      searchKey: 'searchKey',
+      githubUser: 'githubUser',
+    }),
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
